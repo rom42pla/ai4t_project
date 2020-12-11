61cd11ab105bd169e5af769c5238a9fc30705889
dario <1741637.ruggeri@studenti.uniroma1.it>
2020-12-11 12:54:44 +0100

    l'ennesimo test

--- scripts/run_simulation.sh
+++ scripts/run_simulation.sh
@@ -30,10 +30,10 @@ cd ../abides
 # plotting the results: (one image for each symbol)
 cd util/plotting/
 mkdir -p $img_path
-#python -u liquidity_telemetry.py ../../log/$sim_name$seed_num/ExchangeAgent0.bz2 ../../log/$sim_name$seed_num/ORDERBOOK_ETF_FULL.bz2 -o $img_path"ETF_plot.png" -c configs/$plot_config".json"
-#python -u liquidity_telemetry.py ../../log/$sim_name$seed_num/ExchangeAgent0.bz2 ../../log/$sim_name$seed_num/ORDERBOOK_SYM1_FULL.bz2 -o $img_path"SYM1_plot.png" -c configs/$plot_config".json"
-#python -u liquidity_telemetry.py ../../log/$sim_name$seed_num/ExchangeAgent0.bz2 ../../log/$sim_name$seed_num/ORDERBOOK_SYM2_FULL.bz2 -o $img_path"SYM2_plot.png" -c configs/$plot_config".json"
-#python -u liquidity_telemetry.py ../../log/$sim_name$seed_num/ExchangeAgent0.bz2 ../../log/$sim_name$seed_num/ORDERBOOK_SYM2_FULL.bz2 -o $img_path"SYM3_plot.png" -c configs/$plot_config".json"
+python -u liquidity_telemetry.py ../../log/$sim_name$seed_num/ExchangeAgent0.bz2 ../../log/$sim_name$seed_num/ORDERBOOK_ETF_FULL.bz2 -o $img_path"ETF_plot.png" -c configs/$plot_config".json"
+python -u liquidity_telemetry.py ../../log/$sim_name$seed_num/ExchangeAgent0.bz2 ../../log/$sim_name$seed_num/ORDERBOOK_SYM1_FULL.bz2 -o $img_path"SYM1_plot.png" -c configs/$plot_config".json"
+python -u liquidity_telemetry.py ../../log/$sim_name$seed_num/ExchangeAgent0.bz2 ../../log/$sim_name$seed_num/ORDERBOOK_SYM2_FULL.bz2 -o $img_path"SYM2_plot.png" -c configs/$plot_config".json"
+python -u liquidity_telemetry.py ../../log/$sim_name$seed_num/ExchangeAgent0.bz2 ../../log/$sim_name$seed_num/ORDERBOOK_SYM2_FULL.bz2 -o $img_path"SYM3_plot.png" -c configs/$plot_config".json"
 cd ../..
 
 # gets the .csv from the simulation
