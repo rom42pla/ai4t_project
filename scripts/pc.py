41efaa3fae8ce3efe39c5137bc22aec559fcbf90
rom42pla <romeo.lanzino@gmail.com>
2020-12-21 11:36:15 +0100

    Boh

--- scripts/pc.py
+++ scripts/pc.py
@@ -1,37 +1,35 @@
-
 from pprint import pprint
 from os import listdir
 from os.path import join
 
 import numpy as np
 from scipy.spatial import distance_matrix
+from sklearn.neighbors import NearestNeighbors
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 
-
 data_path = join("..", "data")
 sample_simulation_path = join(data_path, "realistic_scenario123456")
-
-pprint(listdir(sample_simulation_path))
-
 
 symbols_names = ["ETF", "SYM1", "SYM2", "SYM3"]
 rows_to_drop = 5000
 mid_prices, transacted_volumes = {}, {}
 for symbol_name in symbols_names:
-    orderbook, orderbook_transacted = pd.read_csv(join(sample_simulation_path, f"ORDERBOOK_{symbol_name}_FULL_processed_orderbook.csv")).rename(columns={'index': 'time'}), \
-                                      pd.read_csv(join(sample_simulation_path, f"ORDERBOOK_{symbol_name}_FULL_transacted_orders.csv")).rename(columns={'index': 'time'})
+    orderbook, orderbook_transacted = pd.read_csv(
+        join(sample_simulation_path, f"ORDERBOOK_{symbol_name}_FULL_processed_orderbook.csv")).rename(
+        columns={'index': 'time'}), \
+                                      pd.read_csv(join(sample_simulation_path,
+                                                       f"ORDERBOOK_{symbol_name}_FULL_transacted_orders.csv")).rename(
+                                          columns={'index': 'time'})
     mid_prices[symbol_name], transacted_volumes[symbol_name] = orderbook["MID_PRICE"].values[rows_to_drop:], \
                                                                orderbook_transacted["SIZE"].values[rows_to_drop:]
-print(mid_prices)
 
-
-sample_indexes = np.linspace(start=0, stop=mid_prices["ETF"].shape[0] - rows_to_drop, num=5,
+sample_indexes = np.linspace(start=0, stop=mid_prices["ETF"].shape[0] - rows_to_drop, num=1000,
                              endpoint=False, dtype=np.int)
-sample_indexes_transacted = np.linspace(start=0, stop=transacted_volumes["ETF"].shape[0] - rows_to_drop, num=5,
-                             endpoint=False, dtype=np.int)
+sample_indexes_transacted = np.linspace(start=0, stop=transacted_volumes["ETF"].shape[0] - rows_to_drop, num=1000,
+                                        endpoint=False, dtype=np.int)
 
 for symbol_name in symbols_names:
     fig, ax = plt.subplots(2)
@@ -41,9 +39,10 @@ for symbol_name in symbols_names:
     mid_price_ax.set(xticklabels=[], xlabel="Time", ylabel="Mid price")
 
     size_ax = sns.lineplot(data=transacted_volumes[symbol_name][sample_indexes_transacted],
-                             ax=ax[1])
+                           ax=ax[1])
     size_ax.set(xticklabels=[], xlabel="Time", ylabel="Transacted volume")
 plt.tight_layout()
+plt.plot()
 
 
 def get_manifold(*time_series, plot=True):
@@ -58,7 +57,7 @@ def get_manifold(*time_series, plot=True):
         # checks that the shapes of each symbol must match with the others
         assert len(time_serie.shape) == 1
         if i_symbol > 0:
-            assert time_series[i_symbol].shape == time_series[i_symbol-1].shape
+            assert time_series[i_symbol].shape == time_series[i_symbol - 1].shape
     manifold = np.stack(time_series, axis=1)
 
     # eventually plot the manifold
@@ -77,6 +76,19 @@ def get_manifold(*time_series, plot=True):
             plt.show()
     return manifold
 
+
+def get_nn_and_distances(manifold, num_nn=5):
+    # distances = distance_matrix(manifold, manifold, p=1)
+    nn_distances, nn_indices = NearestNeighbors(n_neighbors=num_nn + 1, metric="manhattan", algorithm="auto").fit(
+        manifold).kneighbors(manifold)
+    nn_distances, nn_indices = nn_distances[:, 1:], nn_indices[:, 1:]
+
+    w = np.exp(-nn_distances) / \
+        (np.sum(np.exp(-nn_distances)))
+
+    return w, nn_indices
+
+
 def PC(*time_series, E=3, tau=1):
     # eventually unpacks the list
     if len(time_series) == 1:
@@ -89,22 +101,36 @@ def PC(*time_series, E=3, tau=1):
         # checks that the shapes of each symbol must match with the others
         assert len(time_serie.shape) == 1
         if i_symbol > 0:
-            assert time_series[i_symbol].shape == time_series[i_symbol-1].shape
+            assert time_series[i_symbol].shape == time_series[i_symbol - 1].shape
 
     attractor = get_manifold(time_series)
-    manifolds = [get_manifold([time_serie[e*tau:-((E-e-1)*tau) if E-e-1 != 0 else None]
+    manifolds = [get_manifold([time_serie[e * tau:-((E - e - 1) * tau) if E - e - 1 != 0 else None]
                                for e in range(E)], plot=False)
-                  for time_serie in time_series]
-    s = [(manifold[:, :-1] - manifold[:, 1:]) / manifold[:, 1:] for manifold in manifolds]
-    distances = distance_matrix(manifolds[0], manifolds[0], p=1)
-    from sklearn.neighbors import NearestNeighbors
-    neigh = NearestNeighbors(n_neighbors=2, radius=0.4).fit(manifolds[0])
-    print(distances)
-    print(distances.shape)
-    print(np.triu(distances))
+                 for time_serie in time_series]
+    s = [(manifold[:, :-1] - manifold[:, 1:]) / manifold[:, 1:]
+         for manifold in manifolds]
 
-    #w = [np.exp(np.abs()) for manifold in manifolds]
+
+
+
+    w, nn_indices = [], []
+    for manifold in manifolds:
+        nn = get_nn_and_distances(manifold=manifold, num_nn=5)
+        w += [nn[0]]
+        nn_indices += [nn[1]]
+    nn_S = []
+    for i, manifold in enumerate(manifolds):
+        #print(s[i].shape)
+        #print(nn_indices[i].shape)
+        #print(s[i][nn_indices[i]].shape)
+        #print(w[i].shape)
+        print(w[i].shape)
+        print(s[i].shape)
+        #print(w[i].shape, s[i][nn_indices[i], :].shape)
+        nn_S += [np.sum([w[i] * s[i][nn_indices[i]]])]
+
+    # w = [np.exp(np.abs()) for manifold in manifolds]
     print("Ok")
 
-PC([mid_prices[symbol_name][sample_indexes] for symbol_name in symbols_names])
 
+PC([mid_prices[symbol_name][sample_indexes] for symbol_name in symbols_names])
